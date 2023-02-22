ae82be22296a020a2d098a991f0691032f1a35f5
Alexandr <alekspetrofint@gmail.com>
2023-02-22 17:28:43 +0200

    add getWayRateLimit

--- DIcontainerExpress/database.js
+++ DIcontainerExpress/database.js
@@ -1,4 +1,4 @@
-const sqlite3 = require('sqlite3').verbose();
+/* const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./mydatabase.sqlite3', (err) => {
   if (err) {
@@ -10,6 +10,48 @@ const db = new sqlite3.Database('./mydatabase.sqlite3', (err) => {
 });
 
 function query(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+}
+
+module.exports = {
+  query,
+}; */
+const sqlite3 = require('sqlite3').verbose();
+
+class DatabaseBuilder {
+  constructor(dbFilePath) {
+    this.dbFilePath = dbFilePath;
+    this.db = null;
+  }
+
+  open() {
+    this.db = new sqlite3.Database(this.dbFilePath);
+    return this;
+  }
+
+  configure() {
+    // Add any database configuration here
+    return this;
+  }
+
+  build() {
+    return this.db;
+  }
+}
+
+function query(sql, params) {
+  const db = new DatabaseBuilder('mydatabase.sqlite3')
+    .open()
+    .configure()
+    .build();
   return new Promise((resolve, reject) => {
     db.all(sql, params, (err, rows) => {
       if (err) {
