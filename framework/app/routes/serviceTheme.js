7327fbc8e6ce7cc090a81f1eb0a5a15a82db5de7
Alexandr <alekspetrofint@gmail.com>
2023-03-02 14:28:01 +0200

    framework fix route

--- framework/app/routes/serviceTheme.js
+++ framework/app/routes/serviceTheme.js
@@ -1,26 +1,40 @@
+const fs = require("fs");
+const path = require("path");
 class ServiceTheme {
   activeThemeName;
-  theme = [
-    { name: 'default', active: true },
-    { name: 'mythema', active: false },
-  ];
+  themes = fs
+    .readFileSync(path.join(__dirname, "theme.json"), 'utf8')
+  /*   .then(function (result) {      
+      const arr = []
+      arr.push(JSON.parse(result))
+      console.log(arr)
+      return arr;
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); */
+        theme = [
+    { name: "default", active: true },
+    { name: "mythema", active: false }, 
+  ];     
+
   posts = [
     {
-      title: 'My First Blog Post',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      author: 'John Doe',
-      slug: 'my-first-blog-post',
+      title: "My First Blog Post",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      author: "John Doe",
+      slug: "my-first-blog-post",
       excerpt:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia augue vel urna tristique, ac eleifend sapien ultricies.',
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia augue vel urna tristique, ac eleifend sapien ultricies.",
     },
     {
-      title: 'My Second Blog Post',
+      title: "My Second Blog Post",
       content:
-        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      author: 'Jane Smith',
-      slug: 'my-second-blog-post',
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      author: "Jane Smith",
+      slug: "my-second-blog-post",
       excerpt:
-        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     // ...
   ];
@@ -29,7 +43,11 @@ class ServiceTheme {
   }
 
   getActiveTheme() {
-    this.activeThemeName = this.theme.filter(
+    
+    const arr = []
+    arr.push(JSON.parse(this.themes))
+    console.log("theme", arr);
+    this.activeThemeName = arr.filter(
       (theme) => theme.active === true
     )[0];
     return this.activeThemeName;
---
+++ framework/themes/blog/routes.js
@@ -0,0 +1,49 @@
+class getRoutesPagesPath {
+  getRoutes(data) { 
+    const routes = [
+      {
+        method: 'GET',
+        path: '/index',
+        handler: [
+          (req, res) => {
+            res.send('I am blog!');
+          },
+        ],
+      },
+      {
+        method: 'GET',
+        path: '/',
+        handler: [
+          (req, res, next) => {
+            console.log('I am middleware');
+            next();
+          },
+          (req, res) => {
+            res.render(data.pagesPath, { posts: data.posts, showRecentPostsWidget: true });
+          },
+        ],
+      },
+      {
+        method: 'GET',
+        path: '/about',
+        handler: [
+          (req, res) => {
+            res.render(data.pagesPath + '/about', { posts: data.posts, showRecentPostsWidget: true });
+          },
+        ],
+      },
+      {
+        method: 'GET',
+        path: '/blog',
+        handler: [
+          (req, res) => {
+            res.render(data.pagesPath + '/blog', { posts: data.posts });
+          },
+        ],
+      },
+    ];
+    return routes;
+  }
+}
+
+module.exports = getRoutesPagesPath;
